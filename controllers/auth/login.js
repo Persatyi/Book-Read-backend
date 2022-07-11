84e53ea2de63a9fccdf54e24347df89e27c2486b
Oleksii Persatyi <persatyi@ukr.net>
2022-07-11 18:00:24 +0300

    added packeges

---
+++ controllers/auth/login.js
@@ -0,0 +1,5 @@
+const login = async (req, res) => {
+  console.log("Logged in");
+};
+
+module.exports = login;
---
+++ controllers/auth/logout.js
@@ -0,0 +1,5 @@
+const logout = async (req, res) => {
+  console.log("Logged out");
+};
+
+module.exports = logout;
