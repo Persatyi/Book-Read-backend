c099cd6de6656e9ce577c5cb22a99d4a46573af8
Selim Fazylov <selim.fazylov1@gmail.com>
2022-07-12 17:04:12 +0300

    small fix

--- routers/api/users.js
+++ routers/api/users.js
@@ -6,8 +6,7 @@ const { auth: ctrl } = require("../../controllers");
 const { ctrlWrapper } = require("../../helpers");
 const { schemas } = require("../../models");
 
-router.post("/register", validation(schemas.registerUser), ctrlWrapper(ctrl.register)
-);
+router.post("/register", validation(schemas.registerUser), ctrlWrapper(ctrl.register));
 
 router.post("/login", validation(schemas.loginUser), ctrlWrapper(ctrl.login));
 
