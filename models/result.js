5ff7b395b104dd50a388046e488f7588f2dbc870
Oleksii Persatyi <persatyi@ukr.net>
2022-07-13 18:49:23 +0300

    update

--- models/result.js
+++ models/result.js
@@ -1,2 +1,18 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
+
+const resultSchema = Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  pages: {
+    type: Number,
+    required: true,
+  },
+});
+
+const addResult = Joi.object({
+  date: Joi.date().required(),
+  pages: Joi.number().required(),
+});
