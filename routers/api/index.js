993816fba42118fb3d8c33eea725cb6930402deb
Oleksii Persatyi <persatyi@ukr.net>
2022-07-13 12:58:31 +0300

    created

--- routers/api/index.js
+++ routers/api/index.js
@@ -1,9 +1,11 @@
 const users = require("./users");
 const books = require("./books");
 const trainings = require("./trainings");
+const results = require("./results");
 
 module.exports = {
   users,
   books,
   trainings,
+  results,
 };
