9e718d2a5af743758734de91b2d0ec36c9b8c4bf
Yevhen Malyshko <memx86@gmail.com>
2022-07-15 16:21:51 +0300

    getTrining route return 404 error on no active training found

--- services/training/get.js
+++ services/training/get.js
@@ -1,10 +1,12 @@
 const { Training } = require("../../models/training");
+const createError = require("../../helpers");
 
 const get = async (userId) => {
   const trainings = await Training.find({ owner: userId }, "-__v -owner");
   const currentTraining = trainings?.find(
     (training) => training.end > new Date()
   );
+  if (!currentTraining) createError(404, "No active training found");
   return currentTraining;
 };
 
